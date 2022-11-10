5e0809f11eece149d007154e3d988344b513194f
nanashi02 <39255101+nanashi02@users.noreply.github.com>
2022-11-10 16:38:41 -0500

    Done doing Subcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { AddTournamentComponent } from './add-tournament/add-tournament.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTournamentComponent
   ],
   imports: [
     BrowserModule,FormsModule,
