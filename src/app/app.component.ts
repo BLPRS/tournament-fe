641254efddfef1718f3992cc2ec2a49f758a3c26
nanashi02 <39255101+nanashi02@users.noreply.github.com>
2022-11-10 15:49:44 -0500

    Done as a whole
    NEED TO PUT IN SUB COMPONENTS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import { TodoList } from "./todoList";
+// import { TodoItem } from "./todoItem";
 
 @Component({
   selector: 'app-root',
@@ -7,8 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  //NOT USE
-  title = 'BLPRS';
+  // //NOT USE
+  // title = 'BLPRS';
+
 
   //NOT USE
   showComplete: boolean = false;  
@@ -17,30 +20,31 @@ export class AppComponent {
   private tournyList = new ListOfTourny
   ("Username",
     [
-      new ItemTourny("Tekken Tournament","Try-Out Tekken 8 Matchup",false),
-      new ItemTourny("GTA 5v5 Men's Basketball","Greater Ontario young-adults basketball"),
-      new ItemTourny("War On Mobile Legend","Bring the chaos, play with WARZ_MOB"),
+      new ItemTourny("Tekken Tournament","Try-Out Tekken 8 Matchup\n[GT:Double Tournament,GF:Single Elimination,NP:10 players]","2021-05-11"),
+      new ItemTourny("GTA 5v5 Men's Basketball","Greater Ontario young-adults basketball\n[GT:Single Tournament,GF:Round Robin,NP:26 players]","2022-09-22"),
+      new ItemTourny("War On Mobile Legend","Bring the chaos, play with WARZ_MOB [GT:Multiple Tournament,GF:Double Elimination,NP:6 players]","2022-10-8"),
     ]
   );
 
   //NOT USE
-  // get username(): string {    
-  //   return this.tournyList.user;
-  // }  
+  get username(): string {    
+    return this.tournyList.user;
+  }  
 
   get getItems(): ItemTourny[] {    
     return this.tournyList.items.filter( item =>  this.showComplete || !item.isComplete);
+    //return this.tournyList.items;
   }
 
   //NOT USE
-  // get itemCount(): number {
-  //   return this.getItems.length;
-  // }
+  get itemCount(): number {
+    return this.getItems.length;
+  }
 
   /*function to add items*/
-  addNewItem(newItem: string, newItem2: string){
+  addNewItem(newItem: string, newItem2: string,sDate: string){
     if(newItem != ""){
-      this.tournyList.addItem(newItem,newItem2);
+      this.tournyList.addItem(newItem,newItem2,sDate);
     }
   }
 }
@@ -55,8 +59,8 @@ export class ListOfTourny {
       return this.itemsForTourny;
   }
   
-  addItem(task: string,task2: string) {
-      this.itemsForTourny.push(new ItemTourny(task,task2));
+  addItem(task: string,task2: string,sDate: string) {
+      this.itemsForTourny.push(new ItemTourny(task,task2,sDate));
   }
 }
 
@@ -64,11 +68,13 @@ export class ItemTourny{
     
   task: string;
   task2: string;
+  startDate: string | undefined;
   isComplete: boolean;
 
-  constructor(taskVal: string,taskVal2: string, completeVal: boolean = false){
+  constructor(taskVal: string,taskVal2: string,startDate: string, completeVal: boolean = false){
       this.task = taskVal;
       this.task2 = taskVal2;
+      this.startDate = startDate;
       this.isComplete = completeVal;
   }
 }
