a1e1cde7934c52037c3b737f722729b193f04eca
Lanh Hoang <lanhhoang@users.noreply.github.com>
2022-11-11 13:36:26 -0500

    Import header & footer to index

--- src/app/components/index/index.module.ts
+++ src/app/components/index/index.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
+import { FooterModule } from "../footer/footer.module";
+import { HeaderModule } from "../header/header.module";
 import { IndexComponent } from "./index.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HeaderModule, FooterModule],
   declarations: [IndexComponent],
   exports: [IndexComponent],
 })
