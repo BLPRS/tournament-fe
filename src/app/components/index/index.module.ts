1ad57f816a4beaea86d9a97986bf75b402951c9b
Lanh Hoang <lanhhoang@users.noreply.github.com>
2022-11-11 13:57:55 -0500

    Import AddTournament to IndexComponent

--- src/app/components/index/index.module.ts
+++ src/app/components/index/index.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
+import { AddTournamentModule } from "../add-tournament/add-tournament.module";
 import { FooterModule } from "../footer/footer.module";
 import { HeaderModule } from "../header/header.module";
 import { IndexComponent } from "./index.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HeaderModule, FooterModule],
+  imports: [BrowserModule, FormsModule, HeaderModule, FooterModule, AddTournamentModule],
   declarations: [IndexComponent],
   exports: [IndexComponent],
 })
