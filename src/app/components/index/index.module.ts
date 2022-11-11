0604a71d4e3f4ffa926f15bab409649956bc92ab
Lanh Hoang <lanhhoang@users.noreply.github.com>
2022-11-11 17:10:46 -0500

    Update IndexComponent

--- src/app/components/index/index.module.ts
+++ src/app/components/index/index.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
-import { AddTournamentModule } from "../add-tournament/add-tournament.module";
+import { RouterModule } from "@angular/router";
 import { FooterModule } from "../footer/footer.module";
 import { HeaderModule } from "../header/header.module";
 import { IndexComponent } from "./index.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HeaderModule, FooterModule, AddTournamentModule],
+  imports: [BrowserModule, FormsModule, RouterModule, HeaderModule, FooterModule],
   declarations: [IndexComponent],
   exports: [IndexComponent],
 })
