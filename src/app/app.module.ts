c4933fe00aab8d2e6a74e412c307639c33820075
richard101088 <113070613+richard101088@users.noreply.github.com>
2022-11-14 23:36:03 -0500

    v.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from "./app.component";
 
 import { IndexModule } from "./components/index/index.module";
 import { TournamentModule } from "./components/tournament/tournament.module";
+import { MyProfileComponent } from './my-profile/my-profile.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MyProfileComponent],
   imports: [BrowserModule, FormsModule, AppRoutingModule, IndexModule, TournamentModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/my-profile/my-profile.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-my-profile',
+  templateUrl: './my-profile.component.html',
+  styleUrls: ['./my-profile.component.scss']
+})
+export class MyProfileComponent implements OnInit {
+
+title: String
+firstName: String
+lastName: String
+email: String
+username: String
+userpassword: String
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
