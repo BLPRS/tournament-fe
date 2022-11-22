cc755c1bf5e452a986e3f58ed0f2a3e91afd2fb5
Lanh Hoang <lanhhoang@users.noreply.github.com>
2022-11-22 15:16:06 -0500

    Integrate Sign Up API to RegisterComponent

--- src/app/components/auth/register.component.ts
+++ src/app/components/auth/register.component.ts
@@ -1,26 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AuthService } from "src/app/models/auth.service";
+import { User } from "src/app/models/user.model";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./auth.component.scss']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./auth.component.scss"],
 })
 export class RegisterComponent implements OnInit {
+  public user: User = new User();
+  public confirmPassword: string;
+  public message: string;
   isPasswordVisible: boolean = false;
 
-  constructor() {
-  }
+  constructor(private router: Router, private auth: AuthService) {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   get passwordType(): string {
-    return this.isPasswordVisible ? 'text' : 'password';
-  }
-
-  saveUser() {
+    return this.isPasswordVisible ? "text" : "password";
   }
 
   togglePassword() {
     this.isPasswordVisible = !this.isPasswordVisible;
+  }
+
+  signup(form: NgForm) {
+    if (form.valid) {
+      if (this.user.password === this.confirmPassword) {
+        this.auth.signup(this.user).subscribe(response => {
+          console.log(response);
+
+          if (response.success) {
+            alert(response.message);
+            this.router.navigateByUrl("auth/login");
+          }
+          this.message = response.message;
+        })
+      } else {
+        this.message = "Password do not match";
+      }
+    } else {
+      this.message = "Invalid Form Data"
+    }
   }
 }
